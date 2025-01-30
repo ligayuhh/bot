d838e0a9469649496f4d41b7882b51d2d0eb3f1d
raesuzy <raesuzy01@gmail.com>
2025-01-30 23:28:21 +0800

    Update tgbot.py

--- tgbot.py
+++ tgbot.py
@@ -97,7 +97,7 @@ def current_inbox(message):
         return
     messages = get_messages(current_email)
     if messages:
-        formatted_messages = [f"📩 Message #{idx+1} 📩\n📌 ID: {msg['id']}\n✉️ Subject: {msg['subject']}\n👤 From: {msg['sender_name']} <{msg['sender_email']}>\n🕒 Timestamp: {format_timestamp(msg['timestamp']['date'])}" for idx, msg in enumerate(messages)]
+        formatted_messages = [f"📌 ID: {msg['id']}\n✉️ Subject: {msg['subject']}\n👤 From: {msg['sender_name']} <{msg['sender_email']}>\n🕒 Timestamp: {format_timestamp(msg['timestamp']['date'])}" for idx, msg in enumerate(messages)]
         bot.reply_to(message, f"📬 Your Email: {current_email}\n\n" + "\n\n".join(formatted_messages))
     else:
         bot.reply_to(message, f"📬 Your Email: {current_email}\n\nNo messages found in the inbox.")
@@ -110,7 +110,7 @@ def custom_inbox(message):
         return
     messages = get_messages(custom_email)
     if messages:
-        formatted_messages = [f"📩 Message #{idx+1} 📩\n📌 ID: {msg['id']}\n✉️ Subject: {msg['subject']}\n👤 From: {msg['sender_name']} <{msg['sender_email']}>\n🕒 Timestamp: {format_timestamp(msg['timestamp']['date'])}" for idx, msg in enumerate(messages)]
+        formatted_messages = [f"📌 ID: {msg['id']}\n✉️ Subject: {msg['subject']}\n👤 From: {msg['sender_name']} <{msg['sender_email']}>\n🕒 Timestamp: {format_timestamp(msg['timestamp']['date'])}" for idx, msg in enumerate(messages)]
         bot.reply_to(message, f"📬 Your Email: {custom_email}\n\n" + "\n\n".join(formatted_messages))
     else:
         bot.reply_to(message, f"📬 Your Email: {custom_email}\n\nNo messages found in the inbox.")
